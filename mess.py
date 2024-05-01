84ae835279072af00a54e90a597c32d2c91a5cc0
YuanziX <achubadyal4@gmail.com>
2024-05-01 16:37:54 +0530

    initial commit

---
+++ mess.py
@@ -0,0 +1,125 @@
+import re
+import pytz
+import pandas as pd
+from datetime import datetime
+
+time_slot = {
+    (730, 845): "BREAKFAST",
+    (1230, 1345): "LUNCH",
+    (1730, 1830): "SNACKS",
+    (1930, 2045): "DINNER",
+}
+
+
+def _getDateTime():
+    dt = datetime.now(pytz.timezone('Asia/Kolkata'))
+    return [dt.time().hour * 100 + dt.time().minute, dt.date().day]
+
+
+def capitalize(string):
+    if isinstance(string, list):
+        return _capitalize_array(string)
+
+    return _capitalize_string(string)
+
+
+def _capitalize_array(string: list) -> str:
+    for index, item in enumerate(string):
+        string[index] = _capitalize_string(item)
+    return string
+
+
+def _capitalize_string(string: str) -> str:
+    return string[0].upper() + string[1:].lower()
+
+
+def _clean_menu_entry(string: str):
+    return capitalize(re.sub(r"\s+", " ", string))
+
+
+def _meal_name(col: int):
+    return list(time_slot.values())[col]
+
+
+def clean_mess_menu(file_location: str) -> pd.DataFrame:
+    """
+    Reads an messy excel mess menu, cleans it, and saves it as a CSV.
+
+    Args:
+        file_path: Path to the excel file containing the mess menu.
+
+    Returns:
+        dataframe object: Cleaned mess menu.
+    """
+    menu = pd.read_excel(file_location)
+    clean_menu = menu.dropna(axis=0, how="all").dropna(axis=1, how="all")
+    clean_menu.columns = clean_menu.iloc[1].to_list()
+    clean_menu = clean_menu.iloc[2:-1]
+    clean_menu.reset_index(drop=True, inplace=True)
+    clean_menu = clean_menu.replace(r"\s+", " ", regex=True)
+    clean_menu[["DAYS", "DATES"]] = clean_menu.iloc[:,0].str.split(" ", expand=True, n = 1).iloc[
+        :, :2
+    ]
+    try:
+        clean_menu.rename(columns={'BREAK FAST': 'BREAKFAST'}, inplace=True)
+    except:
+        pass
+    clean_menu = clean_menu[
+        ["DAYS", "DATES", "BREAKFAST", "LUNCH", "SNACKS", "DINNER"]
+    ]
+    return clean_menu
+
+
+def next_meal(mess_menu: pd.DataFrame):
+    dt = _getDateTime()
+
+    for index, item in enumerate(time_slot.items()):
+        if dt[0] <= item[0][1]:
+            col = item[1]
+            break
+    else:
+        col = "BREAKFAST"
+        dt[1] += 1
+
+    for index, date in enumerate(mess_menu.DATES):
+        dates = [int(i) for i in date.replace(' ', '').split(",")]
+        if dt[1] in dates:
+            row = index
+            break
+
+    return f"Next meal is {capitalize(col)}. The meal contains {_clean_menu_entry(mess_menu[col][row])}."
+
+
+def add_to_meals(meals: list, col: int, row: int, mess_menu: pd.DataFrame):
+    meal = _meal_name(col)
+    meals.append(f"{capitalize(meal)}: {_clean_menu_entry(mess_menu[meal][row])}")
+
+
+
+def next_four_meals(mess_menu: pd.DataFrame):
+    dt = _getDateTime()
+    meals = []
+    for ind, item in enumerate(time_slot.items()):
+        if dt[0] <= item[0][1]:
+            col = ind
+            break
+    else:
+        col = 0
+        dt[1] += 1
+
+    for ind, date in enumerate(mess_menu.DATES):
+        dates = [int(i) for i in date.replace(' ', '').split(",")]
+        if dt[1] in dates:
+            row = ind
+            break
+    
+    add_to_meals(meals, col, row, mess_menu)
+
+    while len(meals) < 4:
+        col = col + 1
+        if col >= 4:
+            col %= 4
+            row = (row + 1) % mess_menu.shape[0]
+        add_to_meals(meals, col, row, mess_menu)
+
+    return meals
---
+++ mess_bot.py
@@ -0,0 +1,87 @@
+import json
+import os
+import priv_constants
+from telegram import Update
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    ContextTypes,
+    MessageHandler,
+    filters,
+)
+from mess import clean_mess_menu, next_meal, next_four_meals
+
+def read_config():
+    try:
+        with open('mess_constants.json', 'r') as file:
+            config_data = json.load(file)
+            return config_data.get('mess_menu_location', '')
+    except FileNotFoundError:
+        return ''
+
+MESS_MENU_LOCATION = read_config()
+MESS_MENU = clean_mess_menu(MESS_MENU_LOCATION) if MESS_MENU_LOCATION else None
+
+def save_config(mess_menu_location):
+    config_data = {'mess_menu_location': mess_menu_location}
+    with open('mess_constants.json', 'w') as file:
+        json.dump(config_data, file, indent=4)
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send a message when the command /start is issued."""
+    user = update.effective_user
+    await update.message.reply_html(
+        rf"Hi {user.mention_html()}!",
+    )
+
+async def next(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    if not (MESS_MENU is None or MESS_MENU.empty):
+        await update.message.reply_text(next_meal(MESS_MENU))
+    else:
+        await update.message.reply_text("Mess menu is not available.")
+
+
+async def next_four(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    if not (MESS_MENU is None or MESS_MENU.empty):
+        await update.message.reply_text("\n".join(next_four_meals(MESS_MENU)))
+    else:
+        await update.message.reply_text("Mess menu is not available.")
+
+async def use_next(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text("Use /next to get the next meal.\nUse /next_four to get the next four meals.")
+
+async def handle_file(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    global MESS_MENU
+    global MESS_MENU_LOCATION
+    file = update.message.document
+    if file.mime_type == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":
+        try:
+            os.remove(MESS_MENU_LOCATION)
+        except:
+            pass
+        MESS_MENU_LOCATION = f"mess_menus/{file.file_name}"
+        if os.path.exists(MESS_MENU_LOCATION):
+            os.remove(MESS_MENU_LOCATION)
+        file = await update.message.document.get_file()
+        await file.download_to_drive(custom_path=MESS_MENU_LOCATION)
+        save_config(MESS_MENU_LOCATION)
+        MESS_MENU = clean_mess_menu(MESS_MENU_LOCATION)
+        await update.message.reply_text("Mess menu file received and updated successfully.")
+    else:
+        await update.message.reply_text("Please send an Excel file with the mess menu.")
+
+def main() -> None:
+    application = Application.builder().token(TOKEN).build()
+
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("help", use_next))
+    application.add_handler(CommandHandler("next", next))
+    application.add_handler(CommandHandler("next_four", next_four))
+
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.REPLY & ~filters.COMMAND, use_next))
+    application.add_handler(MessageHandler(filters.Document.FileExtension("xlsx"), handle_file))
+
+    application.run_polling(allowed_updates=Update.ALL_TYPES)
+
+if __name__ == "__main__":
+    main()
