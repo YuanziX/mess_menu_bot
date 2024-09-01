cc312673d78003b645a154de2e49323579d69490
YuanziX <achubadyal4@gmail.com>
2024-09-01 14:57:52 +0530

    parser: update for new mess menu format

--- mess.py
+++ mess.py
@@ -1,13 +1,41 @@
+import re
 import pytz
 import pandas as pd
-from datetime import datetime
+from datetime import datetime, timedelta
 
-time_slot = [845, 1345, 1830, 2045]
+time_slot = {900: "BREAKFAST", 1400: "LUNCH", 1830: "SNACKS", 2100: "DINNER"}
+dayOfTheWeek = {
+    0: "MONDAY",
+    1: "TUESDAY",
+    2: "WEDNESDAY",
+    3: "THURSDAY",
+    4: "FRIDAY",
+    5: "SATURDAY",
+    6: "SUNDAY",
+}
 
 
-def _getDateTime():
+def last_day_of_month(any_day):
+    next_month = any_day.replace(day=28) + datetime.timedelta(days=4)
+    return next_month - timedelta(days=next_month.day)
+
+
+def _getTimeWeekDayDateLastDate():
     dt = datetime.now(pytz.timezone("Asia/Kolkata"))
-    return [dt.time().hour * 100 + dt.time().minute, dt.date().day]
+    return (
+        dt.time().hour * 100 + dt.time().minute,
+        dt.date().weekday(),
+        dt.date().day,
+        datetime.date(dt).day,
+    )
+
+
+def _getMealType(time):
+    for key in time_slot.keys():
+        if time < key:
+            return time_slot[key]
+
+    return None
 
 
 def capitalize(string):
@@ -39,56 +67,67 @@ def clean_mess_menu(file_location: str) -> pd.DataFrame:
     """
     menu = pd.read_excel(file_location)
     clean_menu = menu.dropna(axis=0, how="all").dropna(axis=1, how="all")
-    clean_menu.columns = clean_menu.iloc[1].to_list()
-    clean_menu = clean_menu.iloc[2:]
-    clean_menu.reset_index(drop=True, inplace=True)
+    clean_menu.columns = clean_menu.iloc[0].to_list()
+    clean_menu = clean_menu.iloc[1:]
+    clean_menu.index = clean_menu.iloc[:, 0]
+    clean_menu = clean_menu.drop("DAYS", axis=1)
     clean_menu = clean_menu.replace(r"\s+", " ", regex=True)
-    clean_menu[["DAYS", "DATES"]] = (
-        clean_menu.iloc[:, 0].str.split(" ", expand=True, n=1).iloc[:, :2]
-    )
-
     clean_menu.rename(columns={"BREAK FAST": "BREAKFAST"}, inplace=True)
-    clean_menu = clean_menu[["DAYS", "DATES", "BREAKFAST", "LUNCH", "SNACKS", "DINNER"]]
 
-    new_mess_menu = pd.DataFrame()
-
-    for ind, row in clean_menu.iterrows():
-        try:
-            for i in row["DATES"].split(","):
-                new_mess_menu[int(i.strip())] = row
-        except AttributeError:
-            try:
-                new_mess_menu[int(row["DATES"])] = row
-            except ValueError:
-                pass
-
-    return new_mess_menu
+    return clean_menu
 
 
-def get_meal(mess_menu: pd.DataFrame, date: int, meal_index: int):
-    return f"{date} - {mess_menu.loc[:, date].index[meal_index]}: {mess_menu.loc[:, date].iloc[meal_index]}"
+def get_meal(mess_menu: pd.DataFrame, date: int, day: str, meal_type: str):
+    meal_items = mess_menu.loc[day, meal_type]
+
+    parts = re.split(r"(\d\))", meal_items)
+
+    formatted_items = ""
+    i = 1
+    while i < len(parts):
+        number = parts[i].strip() if i < len(parts) else ""
+        item = parts[i + 1].strip() if (i + 1) < len(parts) else ""
+
+        if item:
+            formatted_items += f"{' ' * 8}{number} {item}\n"
+
+        i += 2
+
+    if not re.search(r"\d\)", meal_items):
+        formatted_items = f"{' ' * 8}{meal_items.strip()}"
+
+    return f"{date} - {capitalize(day)}: {capitalize(meal_type)}\n{formatted_items}"
 
 
 def next_n_meals(mess_menu: pd.DataFrame, n: int):
     meals = []
-    dt = _getDateTime()
+    current_time, current_day, current_date, last_date = _getTimeWeekDayDateLastDate()
+    day_count = 0
 
-    for index, item in enumerate(time_slot):
-        if dt[0] <= item:
-            meal_index = index + 2
+    while len(meals) < n:
+        if current_date > last_date:
             break
-    else:
-        meal_index = 2
-        dt[1] += 1
 
-    meals.append(get_meal(mess_menu, dt[1], meal_index))
+        day_index = (current_day + day_count) % 7
+        day = dayOfTheWeek[day_index]
 
-    for i in range(n - 1):
-        if meal_index == 5:
-            meal_index = 2
-            dt[1] += 1
+        if day_count == 0:
+            start_meal = _getMealType(current_time)
         else:
-            meal_index += 1
-        meals.append(get_meal(mess_menu, dt[1], meal_index))
+            start_meal = None
+
+        meal_started = start_meal is None
+        for _, meal_type in time_slot.items():
+            if not meal_started and meal_type == start_meal:
+                meal_started = True
+
+            if meal_started:
+                meal = get_meal(mess_menu, current_date + day_count, day, meal_type)
+                meals.append(meal)
+
+                if len(meals) == n:
+                    return meals
+        meals[-1] = meals[-1] + "\n"
+        day_count += 1
 
     return meals
